ceee9162813a862134176ffa3c219efe2bf47431
Sholpan Bakhtybekkyzy <pasadena2929@gmail.com>
2023-02-14 03:41:25 +0600

    Внесены изменения в файлы settings.py urls.py
    Поменяла название приложения

--- Homework_52/settings.py
+++ Homework_52/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todolist',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'Homework_52.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lessondb',
+        'USER': 'sholpan',
+        'PASSWORD': 'sholpan',
+        'HOST': 'localhost',
+        'PORT': "",
     }
 }
 
@@ -106,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
